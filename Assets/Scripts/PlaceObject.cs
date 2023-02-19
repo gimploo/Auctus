1026511b64e3be52dac9a3549bbb4443864449f5
gimploo <gokulkrishnan949@gmail.com>
2023-02-19 18:37:03 +0530

    Placement Interaction implemented

---
+++ Assets/Scripts/PlaceObject.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
+
+[RequireComponent(typeof(ARRaycastManager), typeof(ARPlaneManager))]
+public class PlaceObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+    private ARRaycastManager raycastManager;
+    private ARPlaneManager planeManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+        planeManager = GetComponent<ARPlaneManager>();
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouch.EnhancedTouchSupport.Enable();
+        EnhancedTouch.Touch.onFingerDown += FingerDown;
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouch.EnhancedTouchSupport.Disable();
+        EnhancedTouch.Touch.onFingerDown -= FingerDown;
+    }
+
+    private void FingerDown(EnhancedTouch.Finger finger)
+    {
+        //Allows only single touch
+        if (finger.index != 0) return;
+
+        if (raycastManager.Raycast(finger.currentTouch.screenPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            foreach (ARRaycastHit hit in hits)
+            {
+                Pose pose = hit.pose;
+                GameObject obj = Instantiate(prefab, pose.position, pose.rotation);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlaneSet.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaneSet : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+    private ARRaycastManager arRaycastManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private bool isPermanentlySetPose = false;
+
+    private void Awake()
+    {
+        arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPermanentlySetPose) return;
+
+        updatePlacementPose();    
+        updatePlacementIndicator();
+    }
+
+    private void OnEnable()
+    {
+        EnhancedTouch.EnhancedTouchSupport.Enable();
+        EnhancedTouch.Touch.onFingerDown += FingerDownSetPlane;
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouch.EnhancedTouchSupport.Disable();
+        EnhancedTouch.Touch.onFingerDown -= FingerDownSetPlane;
+    }
+
+    private void FingerDownSetPlane(EnhancedTouch.Finger finger)
+    {
+        if (isPermanentlySetPose && placementPoseIsValid) return;
+        if (placementPoseIsValid) {
+            isPermanentlySetPose = true;
+            EnhancedTouch.Touch.onFingerDown -= FingerDownSetPlane;
+        }
+    }
+
+    private void updatePlacementPose()
+    {
+        var screenCenter = Camera.main.ViewportToScreenPoint(
+            new Vector3(0.5f, 0.5f)
+        );
+        arRaycastManager.Raycast(
+            screenCenter, 
+            hits,
+            TrackableType.Planes
+        );
+        placementPoseIsValid = (hits.Count > 0);
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+            var cameraForward = Camera.main.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+
+    private void updatePlacementIndicator()
+    {
+        if (placementPoseIsValid) {
+
+            prefab.SetActive(true);
+
+            prefab.transform.SetPositionAndRotation(
+                placementPose.position, 
+                placementPose.rotation
+            );
+        } else {
+            prefab.SetActive(false);
+        }
+    }
+}
