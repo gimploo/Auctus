57c63a3c59e97d48d12d1f0493b0fc5cf8d87405
gimploo <gokulkrishnan949@gmail.com>
2023-02-22 16:00:23 +0530

    text on cube

--- Assets/Scripts/Stack.cs
+++ Assets/Scripts/Stack.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 using EnhancedTouch = UnityEngine.InputSystem.EnhancedTouch;
+using TMPro;
 
 public class Stack : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject prefab;
+    [SerializeField] GameObject prefab;
+    [SerializeField] TMP_InputField cinputText;
+    private TMP_Text displayText = null;
 
+    private string val = "";
     private int top = -1;
     private Vector3 lastPos = default;
 
     List<GameObject> data = new List<GameObject>();
+
+    public void updateInputText()
+    {
+        val = cinputText.text;
+    }
 
     void Start()
     {
@@ -22,22 +31,27 @@ public class Stack : MonoBehaviour
 
     public void push()
     {
-        top = top + 1;
+        if (val == "" || val == " " ) return;
 
+        top = top + 1;
+        // displayText.text = val;
         data.Insert(
             top,
             Instantiate(
-            prefab, 
-            lastPos,
-            Quaternion.LookRotation(
-                new Vector3(
-                    Camera.main.transform.forward.x, 
-                    0, 
-                    Camera.main.transform.forward.z
-                ).normalized
+                prefab, 
+                lastPos,
+                Quaternion.LookRotation(
+                    new Vector3(
+                        Camera.main.transform.forward.x, 
+                        0, 
+                        Camera.main.transform.forward.z
+                    ).normalized
+                )
             )
-        ));
+        );
         lastPos = lastPos + new Vector3(0.0f, prefab.transform.localScale.y, 0.0f);
+        cinputText.text = "";
+        val = "";
     }
 
     public void pop()
